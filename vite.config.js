b50d63bfab931a45c5ec3a57bf1deae0b84ca859
krccfox <212612780@qq.com>
2026-01-10 23:27:41 +0800

    Fix base path for GitHub Pages 1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kittypad-studio/',
+  base: './',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
